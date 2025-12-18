1d38323890c91ea89c9fdf46f57af14866a74033
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-18 12:36:55 +0600

    server setup

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//t1pwlnRgJkjinAhf
+//style-decor-server
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
